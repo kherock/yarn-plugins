1ed79d702eec6b0b47d7c272c668e906c63e68f7
Kyle Herock <kherock@gmail.com>
2022-01-21 14:45:14 -0500

    chore: remove extraneous test files

--- packages/plugin-esm-loader-typescript/esm/test.js
+++
@@ -1,5 +0,0 @@
-const foo = require(`@yarnpkg/core`);
-
-console.log(Object.keys(foo));
-
-console.log(require.main);
--- packages/plugin-esm-loader-typescript/esm/test.ts
+++
@@ -1,3 +0,0 @@
-import * as foo from '@yarnpkg/core';
-
-console.log(Object.keys(foo));
