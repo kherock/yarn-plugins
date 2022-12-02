09deee9198b17f022e922c460b94a45029a8614a
Kyle Herock <k.herock@elsevier.com>
2022-12-01 19:29:11 -0500

    feat(plugin-esm-loader-typescript): support for Yarn 3.4

--- packages/pnp-esm-loader-typescript/sources/hooks/load.ts
+++ packages/pnp-esm-loader-typescript/sources/hooks/load.ts
@@ -1,9 +1,11 @@
-import {load as loadHook} from '@yarnpkg/pnp/lib/esm-loader/hooks/load';
-import * as loaderUtils   from '@yarnpkg/pnp/lib/esm-loader/loaderUtils';
-import fs                 from 'fs';
-import {fileURLToPath}    from 'url';
+import {npath, VirtualFS}               from '@yarnpkg/fslib';
+import {load as loadHook}               from '@yarnpkg/pnp/lib/esm-loader/hooks/load';
+import {WATCH_MODE_MESSAGE_USES_ARRAYS} from '@yarnpkg/pnp/lib/esm-loader/loaderFlags';
+import * as loaderUtils                 from '@yarnpkg/pnp/lib/esm-loader/loaderUtils';
+import fs                               from 'fs';
+import {fileURLToPath, pathToFileURL}   from 'url';
 
-import * as tsLoaderUtils from '../loaderUtils';
+import * as tsLoaderUtils               from '../loaderUtils';
 
 export async function load(
   urlString: string,
@@ -20,6 +22,22 @@ export async function load(
     const format = tsLoaderUtils.getFileFormat(filePath);
     if (!format)
       return nextLoad(urlString, context, nextLoad);
+
+    // https://github.com/nodejs/node/pull/44366/files#diff-f6796082f599554ec3a29c47cf026cb24fc5104884f2632e472c05fe622d778bR477-R479
+    if (process.env.WATCH_REPORT_DEPENDENCIES && process.send) {
+    // At the time of writing Node.js reports all loaded URLs itself so
+    // we technically only need to do this for virtual files but in the
+    // event that ever changes we report everything.
+      const pathToSend = pathToFileURL(
+        npath.fromPortablePath(
+          VirtualFS.resolveVirtual(npath.toPortablePath(filePath)),
+        ),
+      ).href;
+      process.send({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'watch:import': WATCH_MODE_MESSAGE_USES_ARRAYS ? [pathToSend] : pathToSend,
+      });
+    }
 
     const source = await fs.promises.readFile(filePath, `utf8`);
 
